f46b5b01817f764291d7c97e3700c4b45b1d51e6
Kim Namhoon <reaperes105@gmail.com>
2020-03-21 20:04:58 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,9 @@ function App() {
       <img src={stone} className="logo" alt="logo" />
       <h2 className="title">Medium Stone</h2>
       <p className="description">
-        Medium stone 은 바쁜 현대인들이 일상에서 잠시 쉬어 갈 수 있도록 만들어진 공간입니다.
+        미디엄 스톤은 바쁘고 복잡한 도심 한 가운데서 항상 같은 자리에서 묵묵히 중심을 지키고 있는 공간 입니다.
+        고요하게 천천히 쉬었다 가시길 바랍니다.
+        The silence in your Life.
       </p>
     </div>
   );
