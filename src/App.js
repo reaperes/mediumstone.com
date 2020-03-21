8062c14e95612946f1b67e700f1e4b60ed58f68c
reaperes <reaperes105@gmail.com>
2020-03-21 20:13:52 +0900

    Update style

--- src/App.js
+++ src/App.js
@@ -11,7 +11,9 @@ function App() {
       <h2 className="title">Medium Stone</h2>
       <p className="description">
         미디엄 스톤은 바쁘고 복잡한 도심 한 가운데서 항상 같은 자리에서 묵묵히 중심을 지키고 있는 공간 입니다.
+        <br />
         고요하게 천천히 쉬었다 가시길 바랍니다.
+        <br />
         The silence in your Life.
       </p>
     </div>
