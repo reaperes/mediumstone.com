10c6d96f97f1d28f6a92857e22ebe86007409e79
reaperes <reaperes105@gmail.com>
2020-06-06 15:22:24 +0900

    Change main image

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import stone from './stone.png';
+import stone from './mediumstone.png';
 import cx from 'classnames';
 import { isMobile } from 'react-device-detect';
 import './App.css';
@@ -7,10 +7,12 @@ import './App.css';
 function App() {
   return (
     <div className={cx("App", {mobile: isMobile})}>
-      <img src={stone} className="logo" alt="logo" />
-      <h2 className="title">Medium Stone</h2>
+      <img src={stone} className="logo" alt="logo">
+      </img>
       <p className="description">
-        미디엄 스톤은 바쁘고 복잡한 도심 한 가운데서 항상 같은 자리에서 묵묵히 중심을 지키고 있는 공간 입니다.
+        미디엄 스톤은 바쁘고 복잡한 도심 한 가운데서
+        <br />
+        항상 같은 자리에서 묵묵히 중심을 지키고 있는 공간 입니다.
         <br />
         고요하게 천천히 쉬었다 가시길 바랍니다.
         <br />
