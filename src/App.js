608e9bae4a7d6436a3cb651b7e0403667f0aca41
reaperes <reaperes105@gmail.com>
2020-11-15 22:03:57 +0900

    Fix sentence

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
         <div>미디엄 스톤은 바쁘고 복잡한 도심 한 가운데서</div>
         <div>항상 같은 자리에서 묵묵히 중심을 지키고 있는 공간 입니다.</div>
         <div>고요하게 천천히 쉬었다 가시길 바랍니다.</div>
-        <div className="slogan">We love still your smile, we love still your medium.</div>
+        <div className="slogan">We still love your smile, we still love your medium.</div>
       </section>
     </div>
   );
